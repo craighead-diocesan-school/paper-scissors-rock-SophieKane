1117d4aadb4253666f9d38204ed76b31188a7326
SophieKane <99218816+SophieKane@users.noreply.github.com>
2023-06-26 14:09:09 +1200

    Forgot to commit 🙈 but finished it at the end of term I promise

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,92 @@
 <script>
-  import Header from '$lib/Header.svelte'
+  let response = ""
+  let computerAnswer = ""
+  let answers = ["paper", "scissors", "rock"]
+  let result = ""
+
+  function showUserPicture() {
+    response = response.toLowerCase()
+  }
+
+  function getComputerAnswer() {
+    let index = Math.floor(Math.random(0) * answers.length)
+    computerAnswer = answers[index]
+  }
+
+  function getResult() {
+    if (response == "paper") {
+      if (computerAnswer == "paper") {
+        result = "It's a tie!"
+      } else if (computerAnswer == "scissors") {
+        result = "You lost!"
+      } else if (computerAnswer == "rock") {
+        result = "You won!"
+      }
+    } else if (response == "scissors") {
+      if (computerAnswer == "paper") {
+        result = "You won!"
+      } else if (computerAnswer == "scissors") {
+        result = "It's a tie!"
+      } else if (computerAnswer == "rock") {
+        result = "You lost!"
+      }
+    } else if (response == "rock") {
+      if (computerAnswer == "paper") {
+        result = "You lost!"
+      } else if (computerAnswer == "scissors") {
+        result = "You won!"
+      } else if (computerAnswer == "rock") {
+        result = "It's a tie!"
+      }
+    }
+  }
 </script>
 
-<Header />
+<h1>Paper, Scissors, Rock</h1>
 
-<main>
-  <h2>SvelteKit</h2>
+<label>
+  Rock, Paper or Scissors?
+  <input bind:value={response} />
+</label>
 
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
-</main>
+<button on:click={showUserPicture}>Go</button>
 
-<footer>
-  <p>&copy; Craighead Diocesan School 2023</p>
-</footer>
+<br />
+
+{#if response == "paper"}
+  <img src="paper 2.png" alt="paper" />
+{:else if response == "scissors"}
+  <img src="scissors 3.png" alt="scissors" />
+{:else if response == "rock"}
+  <img src="rock 2.png" alt="rock" />
+{:else}
+  <h3>It appears you have not entered a valid answer. Please try again</h3>
+{/if}
+
+<br />
+
+<button on:click={getComputerAnswer}>Generate Computer Answer</button>
+
+<br />
+
+{#if computerAnswer == "paper"}
+  <img src="paper 2.png" alt="paper" />
+{:else if computerAnswer == "scissors"}
+  <img src="scissors 3.png" alt="scissors" />
+{:else if computerAnswer == "rock"}
+  <img src="rock 2.png" alt="rock" />
+{/if}
+
+<br />
+
+<button on:click={getResult}>Get Result</button>
+
+<br />
+
+{result}
+
+<style>
+  img {
+    width: 15em;
+  }
+</style>
