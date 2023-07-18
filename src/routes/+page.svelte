b45ec4532437e86b38fcf72b27d6fba6285a08d3
SophieKane <99218816+SophieKane@users.noreply.github.com>
2023-07-19 11:19:56 +1200

    Updated the coding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,84 @@
 <script>
-  let response = ""
+  let userAnswer = ""
+  let computerAnswer = ""
+  let answers = ["paper", "scissors", "rock"]
+  let result = ""
+  function paper() {
+    userAnswer = "paper"
+    getComputerAnswer()
+  }
+  function scissors() {
+    userAnswer = "scissors"
+    getComputerAnswer()
+  }
+  function rock() {
+    userAnswer = "rock"
+    getComputerAnswer()
+  }
+  function getComputerAnswer() {
+    let index = Math.floor(Math.random(0) * answers.length)
+    computerAnswer = answers[index]
+    getResult()
+  }
+  function getResult() {
+    if (userAnswer == computerAnswer) {
+      result = "It's a draw"
+    } else if (userAnswer == "paper") {
+      if (computerAnswer == "scissors") {
+        result = "Computer wins"
+      } else {
+        result = "User wins"
+      }
+    } else if (userAnswer == "scissors") {
+      if (computerAnswer == "rock") {
+        result = "Computer wins"
+      } else {
+        result = "User wins"
+      }
+    } else {
+      if (computerAnswer == "paper") {
+        result = "Computer wins"
+      } else {
+        result = "User wins"
+      }
+    }
+  }
+</script>
+
+<button on:click={paper}>Paper</button>
+<button on:click={scissors}>Scissors</button>
+<button on:click={rock}>Rock</button>
+
+<br />
+
+{#if userAnswer == "paper"}
+  <img src="paper 2.png" alt="paper" />
+{:else if userAnswer == "scissors"}
+  <img src="scissors 3.png" alt="scissors" />
+{:else if userAnswer == "rock"}
+  <img src="rock 2.png" alt="rock" />
+{/if}
+
+{#if computerAnswer == "paper"}
+  <img src="paper 2.png" alt="paper" />
+{:else if computerAnswer == "scissors"}
+  <img src="scissors 3.png" alt="scissors" />
+{:else if userAnswer == "rock"}
+  <img src="rock 2.png" alt="rock" />
+{/if}
+
+<p>You chose {userAnswer}. The computer chose {computerAnswer}</p>
+<br />
+{result}
+
+<!-- <script>
+  let userAnswer = ""
   let computerAnswer = ""
   let answers = ["paper", "scissors", "rock"]
   let result = ""
 
   function showUserPicture() {
-    response = response.toLowerCase()
+    userAnswer = userAnswer.toLowerCase()
   }
 
   function getComputerAnswer() {
@@ -14,7 +87,7 @@
   }
 
   function getResult() {
-    if (response == "paper") {
+    if (userAnswer == "paper") {
       if (computerAnswer == "paper") {
         result = "It's a tie!"
       } else if (computerAnswer == "scissors") {
@@ -22,7 +95,7 @@
       } else if (computerAnswer == "rock") {
         result = "You won!"
       }
-    } else if (response == "scissors") {
+    } else if (userAnswer == "scissors") {
       if (computerAnswer == "paper") {
         result = "You won!"
       } else if (computerAnswer == "scissors") {
@@ -30,7 +103,7 @@
       } else if (computerAnswer == "rock") {
         result = "You lost!"
       }
-    } else if (response == "rock") {
+    } else if (userAnswer == "rock") {
       if (computerAnswer == "paper") {
         result = "You lost!"
       } else if (computerAnswer == "scissors") {
@@ -46,18 +119,18 @@
 
 <label>
   Rock, Paper or Scissors?
-  <input bind:value={response} />
+  <input bind:value={userAnswer} />
 </label>
 
 <button on:click={showUserPicture}>Go</button>
 
 <br />
 
-{#if response == "paper"}
+{#if userAnswer == "paper"}
   <img src="paper 2.png" alt="paper" />
-{:else if response == "scissors"}
+{:else if userAnswer == "scissors"}
   <img src="scissors 3.png" alt="scissors" />
-{:else if response == "rock"}
+{:else if userAnswer == "rock"}
   <img src="rock 2.png" alt="rock" />
 {:else}
   <h3>It appears you have not entered a valid answer. Please try again</h3>
@@ -84,6 +157,12 @@
 <br />
 
 {result}
+
+<style>
+  img {
+    width: 15em;
+  }
+</style> -->
 
 <style>
   img {
